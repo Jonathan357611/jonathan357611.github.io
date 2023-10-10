f110b918ba08e4b6be39b41312cf97657f7a2aed
jonathanf <j.f@g.c>
2023-10-10 18:19:07 +0200

    Made contacts page more responsive

--- js/calculateContactsSizes.js
+++ js/calculateContactsSizes.js
@@ -1,18 +1,15 @@
 const upper = document.getElementById("contacts_top");
 const bottom = document.getElementById("contacts_bottom");
-const contact_text_content = document.getElementById("contact_text_content").innerHTML;
-const contact_title_content = document.getElementById("contact_title_content").innerHTML;
-
-document.getElementById("contact_text_content").innerHTML = "";
-document.getElementById("contact_title_content").innerHTML = "";
+const contact_text_content = document.getElementById("contact_text_content");
+const contact_title_content = document.getElementById("contact_title_content");
 
 function changePositions() {
     if (window.innerWidth / window.innerHeight < 1) {
-        bottom.innerHTML = contact_text_content;
-        upper.innerHTML = contact_title_content;
+        bottom.innerHTML = contact_text_content.innerHTML;
+        upper.innerHTML = contact_title_content.innerHTML;
     } else {
-        upper.innerHTML = contact_text_content;
-        bottom.innerHTML = contact_title_content;
+        bottom.innerHTML = contact_text_content.innerHTML;
+        upper.innerHTML = contact_title_content.innerHTML;
     }
 }
 
