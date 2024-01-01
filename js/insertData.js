a06cc737894ea8c6d68f096f7ef478e57211d52b
Jonathan Fortmann <jonathan.fortmann@gmail.com>
2024-01-01 23:20:59 +0100

    Fixed age system

--- js/insertData.js
+++ js/insertData.js
@@ -4,10 +4,16 @@ const programing_since_spans = document.querySelectorAll(".programing_since");
 function calculateAge(inputDate) {
     const currentDate = new Date();
     const yearsPassed = currentDate.getFullYear() - inputDate.getFullYear();
-    const monthsPassed = (currentDate.getMonth() - inputDate.getMonth() +
-        (12 * (currentDate.getFullYear() - inputDate.getFullYear()))) % 12;
-    const totalYearsPassed = yearsPassed + (monthsPassed / 12);
-    return totalYearsPassed;
+    const monthsPassed = currentDate.getMonth() - inputDate.getMonth();
+    const daysPassed = currentDate.getDate() - inputDate.getDate();
+
+    if (monthsPassed < 0 || (monthsPassed === 0 && daysPassed < 0)) {
+        const adjustedAge = yearsPassed - 1 + ((12 + monthsPassed) / 12);
+        return parseFloat(adjustedAge.toFixed(2));
+    } else {
+        const age = yearsPassed + (monthsPassed / 12);
+        return parseFloat(age.toFixed(2));
+    }
 }
 
 function calculateProgramingSince(age) {
@@ -17,7 +23,11 @@ function calculateProgramingSince(age) {
 
 function insertAge(age) {
     age_spans.forEach(span => {
-        span.innerText = Math.floor(age);
+        if (span.hasAttribute("floor")) {
+            span.innerText = Math.floor(age);
+        } else {
+            span.innerText = age;
+        }
     })
 }
 
